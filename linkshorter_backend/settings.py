9fc3044e1b17d16c2145bf2e662459c38ec655f8
jovaro <jorgevargas0240@gmail.com>
2024-06-02 21:03:19 -0500

    Updated cors

--- linkshorter_backend/settings.py
+++ linkshorter_backend/settings.py
@@ -36,7 +36,7 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # front url
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200','http://localhost:9876','http://127.0.0.1:4200','http://127.0.0.1:9876']
 
 CORS_ALLOW_CREDENTIALS = True
 
